7f7cfdeaa30155682d1341cb8f8f67e3b8a88096
alex <strim0125@yandex.ru>
2022-02-04 20:59:23 +0300

    1.2

---
+++ src/ru/iegs/p2_2/MyArrayDataException.java
@@ -0,0 +1,21 @@
+package ru.iegs.p2_2;
+
+public class MyArrayDataException extends RuntimeException{
+        private int row;
+        private int col;
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getCol() {
+            return col;
+        }
+
+    public MyArrayDataException (String message, int row, int col){
+            super(message);
+            this.row = row;
+            this.col = col;
+
+        }
+}
---
+++ src/ru/iegs/p2_2/MyArraySizeException.java
@@ -0,0 +1,4 @@
+package ru.iegs.p2_2;
+
+public class MyArraySizeException extends RuntimeException{
+}
